84de547759e849998912e2c2609782f873050300
pinhenzhang <pinhenzhang@tencent.com>
2024-10-25 15:14:17 +0800

    use nextjs api router to get data

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,14 +1,10 @@
 'use client';
 
-import SharedLayout from '@/components/SharedLayout/SharedLayout';
+// import SharedLayout from '@/components/SharedLayout/SharedLayout';
 import 'tdesign-react/dist/tdesign.css';
 import { StoreProvider } from '../StoreProvider';
 import { MyLayoutProps } from '@/app/lib/common';
 
 export default function RootLayout({ children }: MyLayoutProps) {
-  return (
-    <StoreProvider>
-      <SharedLayout>{children}</SharedLayout>
-    </StoreProvider>
-  );
+  return <StoreProvider>{children}</StoreProvider>;
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 import 'tdesign-react/dist/tdesign.css';
 import { MyLayoutProps } from '@/app/lib/common';
+import SharedLayout from '@/components/SharedLayout/SharedLayout';
 
 export default async function RootLayout({ children }: MyLayoutProps) {
-
   return (
     <html>
-      <body>{children}</body>
+      <body>
+        <SharedLayout>{children}</SharedLayout>
+      </body>
     </html>
   );
 }
