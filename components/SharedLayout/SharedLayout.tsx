c315a936e304a0cd1b1d711b699aaec8a293dc81
pinhenzhang <pinhenzhang@tencent.com>
2024-10-28 11:30:55 +0800

    修复layout样式问题

--- components/SharedLayout/SharedLayout.tsx
+++ components/SharedLayout/SharedLayout.tsx
@@ -1,17 +1,18 @@
 'use client';
 
+import { MyLayoutProps } from '@/app/lib/common';
 import FooterNav from '@/components/FooterNav/FooterNav';
 import HeaderNav from '@/components/HeaderNav/HeaderNav';
 import Navigation from '@/components/Navigation/Navigation';
 import 'tdesign-react/dist/tdesign.css';
-import {Layout} from 'tdesign-react/lib';
-import {MyLayoutProps} from "@/app/lib/common";
+import { Layout } from 'tdesign-react';
+import Style from './SharedLayout.module.css';
 
 const { Content, Aside } = Layout;
 
 const SharedLayout = ({ children }: MyLayoutProps) => {
   return (
-    <Layout>
+    <Layout className={Style.panel}>
       <HeaderNav></HeaderNav>
       <Layout>
         <Aside style={{ borderTop: '1px solid var(--component-border)' }}>
